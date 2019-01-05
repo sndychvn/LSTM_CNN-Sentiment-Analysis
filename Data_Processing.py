d104e663b6c4386f756eee512bd8e0262578e38d
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2019-01-05 21:53:53 +0100

    *update*

--- Data_Processing.py
+++ Data_Processing.py
@@ -49,6 +49,50 @@ def load_data(posfile, negfile, max_limit, randomize=True):     #get the datset
 
     pos_labels = [[0,1] for _ in pos_file]                      #generates label if a list "pos_file" has 200 values, a pos_lables of 200 [0,1] is generated
     neg_labels = [[1,0] for _ in neg_file]
+    y = np.concatenate([pos_labels, neg_labels],0)
+    return [x_text,y]
+
+
+def clean_data(string):
+
+    string = re.sub(r":\)", "emojihappy1", string)              # emojis cleanup
+    string = re.sub(r":P", "emojihappy2", string)
+    string = re.sub(r":p", "emojihappy3", string)
+    string = re.sub(r":>", "emojihappy4", string)
+    string = re.sub(r":3", "emojihappy5", string)
+    string = re.sub(r":D", "emojihappy6", string)
+    string = re.sub(r" XD ", "emojihappy7", string)
+    string = re.sub(r" <3 ", "emojihappy8", string)
+    string = re.sub(r":\(", "emojisad9", string)
+    string = re.sub(r":<", "emojisad10", string)
+    string = re.sub(r":<", "emojisad11", string)
+    string = re.sub(r">:\(", "emojisad12", string)
+    string = re.sub(r"(@)\w+", "mentiontoken", string)          # hashtags, etc cleanup "(@)\w+"
+    string = re.sub(r"http(s)*:(\S)*", "linktoken", string)     # websites cleanup
+    string = re.sub(r"\\x(\S)*", "", string)                    # unwanted unicode cleanup \x...
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)      # General Cleanup and Symbols
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+
+    return string.strip().lower()
+
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):        #generates a batch iterator for a dataset
+    data = np.array(data)
+    data_size = len(data)
+
+
+
 
 
 
