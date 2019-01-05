814f7edf0efb28a2fd8b246bcf07c24758e835e3
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2019-01-05 21:09:37 +0100

    *update*

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,8 +1,7 @@
 import csv
+import random as rd
 
-
-#seperates the mixed file into positive and negative
-def split_data(filename):
+def split_data(filename):                                  #seperates the mixed file into positive and negative
     positive_file = open("positive_"+filename,"w+")        #creates empty files to store the
     negative_file = open("negative_"+filename, "w+")       #positive and the negative sentences accordingly
 
@@ -17,10 +16,38 @@ def split_data(filename):
             sentiment = row[1]
             sentence = row[3]
 
-            if (sentiment == "0"):
+            if (sentiment=="0"):                           #seperates the sentences to files and writes them to file based on sentiment
                 negative_file.write(sentence+"\n")
+            else:
+                positive_file.write(sentence+"\n")
+
+            if(check%10000==0):
+                print(check)
+
+    positive_file.close()
+    negative_file.close()
 
 
+
+def load_data(posfile, negfile, max_limit, randomize=True):     #get the datset
+    pos_file = list(open(posfile, "r").readlines())             #opening the file from posfile and removing all the unwanted spaces and storing in pos_file
+    pos_file = [s.strip() for s in pos_file]
+
+    neg_file = list(open(negfile, "r").readlines())             #opening the file from negfile and removing all the unwanted spaces and storing in neg file
+    neg_file = [s.strip() for s in neg_file]
+
+    if (randomize):
+        rd.shuffle(pos_file)
+        rd.shuffle(neg_file)
+
+    pos_file = pos_file[:max_limit]
+    neg_file = neg_file[:max_limit]
+
+    x_text = pos_file + neg_file                                #splits data by words
+    x_text = [clean_data(s) for s in x_text]
+
+
+    pos_file =
 
 
 
