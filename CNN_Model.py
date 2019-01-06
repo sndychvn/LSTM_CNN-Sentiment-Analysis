4b50eb1556241c8ab7c2fefde3b61e101cffad14
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2019-01-06 01:22:51 +0100

    *update*

--- CNN_Model.py
+++ CNN_Model.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-class CNN(object):              #Used an embedding layer, a Convolutional Layer, max-pooling and Softmax layer
-
+class CNN(object):
+                                 ##Used an embedding layer, a Convolutional Layer, max-pooling and Softmax layer
     def __init__(self,sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")        #Placeholders for input, output and dropout
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
@@ -20,7 +20,17 @@ class CNN(object):              #Used an embedding layer, a Convolutional Layer,
                 #Convolution Layer
                 filter_shape = [filter_size, embedding_size, 1, num_filters]
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv = tf.nn.conv2d(self.embedded_chars_expanded, W, strides=[1, 1, 1, 1], padding="VALID", name="conv")
+
+                h = tf.nn.relu(tf.nn.bias_add(conv,b), name="relu")                                #apply nonlinearity
+                pooled = tf.nn.max_pool(h, ksize=[1, sequence_length - filter_size + 1, 1, 1], strides=[1, 1, 1, 1], padding='VALID', name="pool")
+                pooled_outputs.append(pooled)
+
+        num_filters_tool = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_tool])
+
 
 
 
