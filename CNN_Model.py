f41c1422e58d5ffbd38a8ac8271cd6b7e760a6d1
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2019-01-06 12:01:33 +0100

    update

--- CNN_Model.py
+++ CNN_Model.py
@@ -19,10 +19,10 @@ class CNN(object):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
                 #Convolution Layer
                 filter_shape = [filter_size, embedding_size, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")         #creating a 4D Tensor
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                conv = tf.nn.conv2d(self.embedded_chars_expanded, W, strides=[1, 1, 1, 1], padding="VALID", name="conv")
-                h = tf.nn.relu(tf.nn.bias_add(conv,b), name="relu")                               #apply nonlinearity
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")         #creating a 4D Tensor for weights
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")                 #creating a bias with 0.1 as value with a shape equal to number of filters (i.e if num_filters = 100) shape would be (100,1)
+                conv = tf.nn.conv2d(self.embedded_chars_expanded, W, strides=[1, 1, 1, 1], padding="VALID", name="conv")        #providing a 4D Tensor to the CONV Layer
+                h = tf.nn.relu(tf.nn.bias_add(conv,b), name="relu")                               #apply non-linearity
                 pooled = tf.nn.max_pool(h, ksize=[1, sequence_length - filter_size + 1, 1, 1], strides=[1, 1, 1, 1], padding='VALID', name="pool")
                 pooled_outputs.append(pooled)
 
@@ -31,11 +31,29 @@ class CNN(object):
         self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_tool])
 
 
+        with tf.name_scope("dropout"):                                                             #adding dropout
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
 
 
+        with tf.name_scope("output"):                                                               #Final (unnormalized) scores and predictions
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
 
+        with tf.name_scope("loss"):                                                                 # Calculate Mean cross-entropy loss
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
-
+        with tf.name_scope("accuracy"):                                                             # Accuracy
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+        print("CNN is Loaded!")
 
 
